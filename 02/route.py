e0789f5e335fa67d01588d200894262ee09f7095
skygemstone <skygemstone@gmail.com>
2019-09-20 16:51:37 +0800

    second commit

---
+++ 02/route.py
@@ -0,0 +1,17 @@
+from flask import render_template
+
+def index():
+    name = {'username': 'root'}
+    posts = [
+        {
+            'author': {'username': 'root'},
+            'body': "hi I'm root!"
+        },
+        
+            'author': {'username': 'test'},
+            'body': "hi I'm test!"
+        },
+
+    ]
+
+return render_template('index.html', name=name, posts= posts)
